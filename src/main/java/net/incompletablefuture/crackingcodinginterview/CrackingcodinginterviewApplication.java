d1b5331b048751e2319b5e0444932ddf68bdf871
dzharkov <dzharkov@phoenixit.ru>
2020-12-22 13:49:38 +0300

    Initial commit

---
+++ src/main/java/net/incompletablefuture/crackingcodinginterview/CrackingcodinginterviewApplication.java
@@ -0,0 +1,13 @@
+package net.incompletablefuture.crackingcodinginterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrackingcodinginterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrackingcodinginterviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/incompletablefuture/crackingcodinginterview/CrackingcodinginterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package net.incompletablefuture.crackingcodinginterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrackingcodinginterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
